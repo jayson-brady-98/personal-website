e9bdece48c09f5a4125aeaaa3a462d009f8eb110
jayson-brady-98 <jaysonbrady123@gmail.com>
2024-12-10 08:56:00 +1100

    About page background fixed

--- components/rotating-background.tsx
+++ components/rotating-background.tsx
@@ -1,29 +1,30 @@
 "use client"
 
-import { useScroll, motion, useTransform, HTMLMotionProps } from "framer-motion"
+import { useScroll, motion, useTransform } from "framer-motion"
 import { useRef } from "react"
 
 export function RotatingBackground({ className }: { className?: string }) {
   const ref = useRef<HTMLDivElement>(null)
   const { scrollY } = useScroll()
   
-  const backgroundY = useTransform(scrollY, [0, 3000], [0, -800])
+  const backgroundY = useTransform(scrollY, [0, 3000], [0, -1200])
 
   return (
-    <motion.div
-      ref={ref}
-      className={`fixed -z-10 ${className ?? ''}`}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      style={{
-        backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(/backgrounds/bg5.jpg)`,
-        backgroundSize: 'cover',
-        backgroundPosition: '50% 225px',
-        backgroundRepeat: 'no-repeat',
-        y: backgroundY,
-        top: 0,
-        bottom: '-300px'
-      }}
-    />
+    <div className={`fixed inset-0 -z-10 ${className ?? ''}`}>
+      <motion.div
+        ref={ref}
+        className="absolute inset-0"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        style={{
+          backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(/backgrounds/bg5.jpg)`,
+          backgroundSize: 'cover',
+          backgroundPosition: '50% 225px',
+          backgroundRepeat: 'no-repeat',
+          y: backgroundY,
+          height: '200vh'
+        }}
+      />
+    </div>
   )
 } 
